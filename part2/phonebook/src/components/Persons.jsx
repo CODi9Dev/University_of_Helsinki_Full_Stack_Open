6ab7e0f8a6e46460d084044c264922d13f4fa8f0
Jordi Martínez Amat <jmamat@gmail.com>
2025-10-20 22:11:18 +0200

    Add exercise 2.11 from 'part2' to the phonebook project

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,7 +1,7 @@
 const Persons = ({ personsToShow }) => {
   return (
     <>
-      {personsToShow.map((person) => <div key={person.name}>{person.name} {person.phone}</div>)}
+      {personsToShow.map((person) => <div key={person.id}>{person.name} {person.number}</div>)}
     </>
   )
 }
